e571faf0432fd82eee32178bb361180851e7ed7f
Adria <2704adria@gmail.com>
2023-11-13 18:56:28 +0100

    base de dades

---
+++ bbdd/excursio.sql
@@ -0,0 +1,85 @@
+-- MariaDB dump 10.19-11.3.0-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: excursio
+-- ------------------------------------------------------
+-- Server version	11.3.0-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alumnes`
+--
+
+DROP TABLE IF EXISTS `alumnes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alumnes` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Nom` varchar(50) NOT NULL,
+  `Cognoms` varchar(100) NOT NULL,
+  `DataNaixement` varchar(255) DEFAULT NULL,
+  `Carrer` varchar(100) NOT NULL,
+  `Numero` int(11) NOT NULL,
+  `Ciutat` varchar(50) NOT NULL,
+  `CodiPostal` varchar(10) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alumnes`
+--
+
+LOCK TABLES `alumnes` WRITE;
+/*!40000 ALTER TABLE `alumnes` DISABLE KEYS */;
+INSERT INTO `alumnes` VALUES
+(2,'Adria','Garcia','2004-04-27','Carrer Nou',90,'Figueres','17600'),
+(3,'David','Gil','2004-05-12','Carrer Tapis',23,'Figueres','17600'),
+(4,'Arnau','Mendez','2004-02-03','Carrer Muntaner',6,'Figueres','17600');
+/*!40000 ALTER TABLE `alumnes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `identificacio`
+--
+
+DROP TABLE IF EXISTS `identificacio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `identificacio` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `CodiAcces` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `identificacio`
+--
+
+LOCK TABLES `identificacio` WRITE;
+/*!40000 ALTER TABLE `identificacio` DISABLE KEYS */;
+INSERT INTO `identificacio` VALUES
+(1,'clauexcursio');
+/*!40000 ALTER TABLE `identificacio` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-13 18:55:29
